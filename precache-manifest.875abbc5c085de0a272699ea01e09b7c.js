d11d8755ff37b6ecbbf042f33bd883a8735bc58e
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2020-08-08 00:47:04 +0400

    Updates

---
+++ precache-manifest.875abbc5c085de0a272699ea01e09b7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2547fbb582e2d13b96d851e87ae1724f",
+    "url": "/covid-19-react/index.html"
+  },
+  {
+    "revision": "ff09eb06d91810ea8727",
+    "url": "/covid-19-react/static/css/2.41f498e0.chunk.css"
+  },
+  {
+    "revision": "8253437f5acda25a42ab",
+    "url": "/covid-19-react/static/css/main.577606ab.chunk.css"
+  },
+  {
+    "revision": "ff09eb06d91810ea8727",
+    "url": "/covid-19-react/static/js/2.9ee056f9.chunk.js"
+  },
+  {
+    "revision": "3ad76833de97695745f4fc6932481651",
+    "url": "/covid-19-react/static/js/2.9ee056f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8253437f5acda25a42ab",
+    "url": "/covid-19-react/static/js/main.da52d457.chunk.js"
+  },
+  {
+    "revision": "141cbafc656f09aefee4",
+    "url": "/covid-19-react/static/js/runtime-main.f79d140c.js"
+  }
+]);
